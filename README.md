b1524c230d07be0fe83e3e0e0964f84b3f3e4051
GideonBature <infoaboutgideon@gmail.com>
2024-09-08 06:21:01 +0100

    getting the inputs and storing in a variable

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mgrep
+
+## Steps
+- Get inputs
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// import the env module
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Read the arguments passed to the program
+    let args: Vec<String> = env::args().collect();
+
+    // Print the arguments
+    dbg!(&args);
+
+    // Save the arguments in variables
+    let arg1 = &args[1];
+    let arg2 = &args[2];
+
+    println!("arg1: {}, arg2: {}", arg1, arg2);
+
+
 }
